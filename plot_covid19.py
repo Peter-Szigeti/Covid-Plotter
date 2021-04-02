3f736762b9526c608315a7958fee3090b0bba2f2
gladiathorer123 <62214014+gladiathorer123@users.noreply.github.com>
2021-04-02 19:33:27 +0200

    Name change

--- plot_covid19.py
+++ plot_covid19.py
@@ -1,12 +1,15 @@
 import os#a mappák létrehozásához és kezeléséhez
+from datetime import date #fájlok elnevezéséhez
 import requests#webscraping
 from bs4 import BeautifulSoup#webscraping
 import pandas as pd
 import urllib.request #a napi halálozásokat tartalmazó táblázathoz kell
 import math
 from matplotlib import pyplot as plt
+
 '''
-Útmutatás a programhoz:tedd be egy mappába és futtasd le:)
+Dependencies: Matplotlib,BeautifulSoup, Pandas
+Útmutatás a programhoz:tedd be egy mappába a .py filet és futtasd le és futtasd le:)
 
 '''
 
@@ -29,11 +32,11 @@ def scrapeUrl(url):
         temp = pd.read_html((url + '?page={}').format(i))[0]
         mylist.append(temp)
     df = pd.concat(mylist)
-    df.to_csv('Data/covid19Deaths_scraped.csv')
+    df.to_csv('Data/covid19Deaths_scraped_{}.csv'.format(date.today().strftime('%Y-%m-%d')))
 
 def downloadTable(url):
     print('Beginning file download with urllib2...')
-    urllib.request.urlretrieve(url, 'Data/covid19HungaryStatistics.xlsx')
+    urllib.request.urlretrieve(url, 'Data/covid19HungaryStatistics_{}.xlsx'.format(date.today().strftime('%Y-%m-%d')))
 
 def initFoldersAndData(url):
     initFolders()
@@ -41,8 +44,8 @@ def initFoldersAndData(url):
     downloadTable('https://docs.google.com/spreadsheets/d/1e4VEZL1xvsALoOIq9V2SQuICeQrT5MtWfBm32ad7i8Q/export?format=xlsx&gid=311133316')
 
 def dateTheScrape():
-    df = pd.read_csv('Data\covid19Deaths_scraped.csv')
-    dates = pd.read_excel('Data\covid19HungaryStatistics.xlsx')
+    df = pd.read_csv('Data/covid19Deaths_scraped_{}.csv'.format(date.today().strftime('%Y-%m-%d')))
+    dates = pd.read_excel('Data/covid19HungaryStatistics_{}.xlsx'.format(date.today().strftime('%Y-%m-%d')))
     df['Date'] = 'hi'
 
     c = 0
@@ -51,6 +54,7 @@ def dateTheScrape():
             for j in range(int(dates['Az új elhunytak száma naponta'][i])):
                 df['Date'][len(df)-1-c] = dates['Dátum'][i]
                 c+=1
+    df.to_csv('Data/covid19Deaths_scraped_DATED_{}.csv'.format(date.today().strftime('%Y-%m-%d')))
     return df
 
 def deathcountPerAgeGroup(step):
@@ -93,7 +97,7 @@ def plot_deathcountPerAgeGroup(df):
     fig.set_size_inches(18.5, 10.5)
     df.plot.area(x = 'Dátum',xlabel = 'Dátum',ylabel = 'Halálozások száma',title = 'Napi COVID19 Halálozások Korcsoportonként Magyarországon',stacked = True,ax=ax)
 
-    plt.savefig('Results/deathcountPerAgeGroup_{}.png'.format(df['Dátum'][0]),dpi=300)
+    plt.savefig('Results/deathcountPerAgeGroup_{}.png'.format(date.today().strftime('%Y-%m-%d')))
 
 url = "https://koronavirus.gov.hu/elhunytak"
 initFoldersAndData(url)
